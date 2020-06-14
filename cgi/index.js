4073e7262522169059236f16ca0e4d86b7efd21d
Vakhtangi Tsvariani <tsvari@gmial.com>
2020-06-14 16:39:49 -0500

    fix file paths

--- cgi/index.js
+++ cgi/index.js
@@ -1,11 +1,33 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var fs = require('fs');
 
 // viewed at http://localhost:8080
 app.get('/', function (req, res) {
-    res.sendFile(path.join('/home/givi/dev/StudentJS/www/index.htm'));
+    var indexFilePath = path.join(__dirname, "../www", "index.htm");
+    fs.access(indexFilePath, fs.F_OK, (err) => {
+        if (err) {
+            console.error(err);
+            res.send("Error to finding index file");
+        return;
+        }
+        //file exists
+        res.sendFile(indexFilePath);
+    })
+    // or use this
+    /*
+    try {
+        if (fs.existsSync(path)) {
+        //file exists
+        }
+    } catch(err) {
+        console.error(err)
+    }
+    */
+    //res.sendFile(path.join('/home/givi/dev/StudentJS/www/index.htm'));
 });
 
 app.listen(8080);
 console.log('Server running at http://127.0.0.1:8080/'); //
+
--- cgi/indexPhone.js
+++ cgi/indexPhone.js
@@ -4,11 +4,12 @@ const path = require('path');
 const fs = require('fs');
 const url = require('url');
 
-// viewed at http://localhost:8080
+// viewed at http://localhost:8091
 app.get('/', function (req, res) {
+    var indexFilePath = path.join(__dirname, "../www", "indexPhone.htm");
     console.log("indexPhone"); //
     var infoFromURL = url.parse(req.url, true).query;
-    fs.readFile('/home/givi/dev/StudentJS/www/indexPhone.htm', 'utf-8', function (err, data) {
+    fs.readFile(indexFilePath, 'utf-8', function (err, data) {
         var dataToString = data.toString();
         var replaced = dataToString.replace('@uidNumberHere', infoFromURL.studentUid);
         res.write(replaced);
